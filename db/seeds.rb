e9278fd6c17b2c0752c71a0d62816a486e2d1a3e
Andy Wooten <andrewdwooten@icloud.com>
2017-03-14 13:11:11 -0600

    Adds seeds
    Adds to seed file for running spec harness.
    Not a perfect application presently; we'll
    need to pull in the production data soon, so
    we can view real outcomes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+STATUS = ["Ordered", "Shipped", "Paid", "Cancelled"]
+  4843.times do
+    Customer.create(first_name: Faker::Name.first_name,
+                    last_name: Faker::Name.last_name)
+  end
+
+  4843.times do
+    Merchant.create(name: Faker::Company.name)
+  end
+  4843.times do
+    Invoice.create(customer_id: Faker::Number.between(1, 4843),
+                   merchant_id: Faker::Number.between(1, 4843),
+                   status: STATUS.sample)
+  end
